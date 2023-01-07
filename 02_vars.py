16ef8a61ecf66dcdfef3c8dfb2578f0b52e090c1
Santiago Gonzalez <parrasanti.gonzalez@gmail.com>
2023-01-06 19:17:51 -0500

    vimos variables y tipos de datos en python

---
+++ 02_vars.py
@@ -0,0 +1,21 @@
+print("Hola, Variables")
+
+# variable con un string
+my_name = "Santiago"
+print(my_name)
+
+
+# variable con un entero
+my_age = 19
+print(my_age)
+
+
+# aqui reasigno valor de la variable "my_age"
+my_age = 20
+print("la edad ahora es", my_age)
+
+
+#solicitando e imprimiendo valores desde la terminal
+
+input_name = input('¿Cual es tu nombre?')
+print("su nombre es", input_name)
---
+++ 03_types.py
@@ -0,0 +1,22 @@
+#strings (cadenas de caracteres)
+my_name = "Santiago"
+
+#int (numeros enteros)
+my_age = 19
+print('mi edad es', my_age)
+print(type(my_age))
+
+#boolean (verdadero o falso)
+is_single = False
+print('es soltero?', is_single)
+print(type(is_single))
+
+#una nueva función de python es "type" la cual nos permite saber que tipo de dato contiene la variable que se le para como parámetro
+print('mi nombre es', my_name)
+print(type(my_name))
+
+#inputs (solicitar información) siempre cuando se ingresa un valor por consola el valor ingresa como string, así se ingrese un número
+
+your_age = input('¿Cual es tu edad?')
+print('tu edad es:', your_age)
+print(type(your_age))
