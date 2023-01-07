6ddb2076a990534faff8f04feb114103349ffe93
Santiago Gonzalez <parrasanti.gonzalez@gmail.com>
2023-01-07 14:46:36 -0500

    Se agrega un archivo explicando los numeros en python

---
+++ 05_numbers.py
@@ -0,0 +1,19 @@
+lives = 3
+age = 12
+budget = 100
+temperature = 12.12
+
+
+print(type(lives))
+print(type(temperature))
+
+#Tambien podemos sumarle o restarle en uno a una variable cuando queramos 
+
+print(lives)
+lives += 1
+print(lives)
+
+#Cuando tenemos un número muy grande python nos lo mostrara con notación científica
+
+big_number = 40000000000000000.1
+print(big_number)
