42cfdd541200dca3bd382c91c976625a45a5593b
Santiago Gonzalez <parrasanti.gonzalez@gmail.com>
2023-01-07 14:53:36 -0500

    Se trató el tema de los booleanos

---
+++ 06_boolean.py
@@ -0,0 +1,8 @@
+#Los booleanos nos permiten diferenciar de verdadero o falso
+is_single = False
+print(type(is_single))
+
+#Podemos invertir el booleano utilizando "not" (tanbien se puede usar en print)
+
+is_single = not is_single
+print(is_single)
