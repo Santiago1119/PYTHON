c74cc6e7934db75f5c9fc7f072102963bff50d03
Santiago Gonzalez <parrasanti.gonzalez@gmail.com>
2023-01-09 13:19:11 -0500

    Se agregó un programa que determina si un número es par o impar

---
+++ numeroPar.py
@@ -0,0 +1,7 @@
+#Programa que determine si un número es par o impar
+numeroIngresado = int(input('Ingresa tu número: '))
+
+if(numeroIngresado % 2 == 0):
+    print('El número ingresado es par')
+else:
+    print('El número ingresado es impar')
