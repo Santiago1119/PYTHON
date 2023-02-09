23e53fa086c21f942e7561038d058e90ad9bf1cc
Santiago Gonzalez <parrasanti.gonzalez@gmail.com>
2023-02-08 20:55:13 -0500

    Se creo carpeta de fundamentos y Comprehesion...

---
+++ Comprehensions, funciones y manejo de errores/01_conjuntos.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Los conjuntos se pueden modificar
+No tienen un orden específico
+No se permiten duplicados
+
+Se crean con corchetes, pero los sets no tienen llaves
+
+
+
+"""
+
+set_countries = {'col', 'mex', 'bol'}
+print(set_countries)
+print(type(set_countries))
+
+
