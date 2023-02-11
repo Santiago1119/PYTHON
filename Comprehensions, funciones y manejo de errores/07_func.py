2d848078ea500def18318df4b0e4fb23851bfaba
Santiago Gonzalez <parrasanti.gonzalez@gmail.com>
2023-02-10 22:24:35 -0500

    Se vio video de funciones en platzi y se aprendio de API's en Flask

---
+++ Comprehensions, funciones y manejo de errores/07_func.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+
+
+
+"""
+
+def my_print(text):
+    print(text * 2)
+    
+    
+my_print('ola ')
+
+def suma(a, b):
+    print(a + b)
+    
+suma(1, 5)
+
+
---
+++ Comprehensions, funciones y manejo de errores/08_return.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+
+"""
+
+def sum_with_range(minimum, maximum):
+    sumatory = 0
+
+    for x in range(minimum, maximum):
+        sumatory += x
+        
+    return sumatory
+    
+sum_with_range(1, 19)
+
+
+
+
